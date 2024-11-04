6be98c3d3b245537a54aad55ab98754ed7c1cd47
GAbriElaJnbg30 <21240200@leon.tecnm.mx>
2024-11-03 22:31:42 -0600

    Ajustes de test_api.sh

--- test_api.sh
+++ test_api.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Credenciales
+USERNAME="goretti"
+PASSWORD="gn300803"
+
 # Endpoint base
 BASE_URL="http://localhost:8080/actividades"
 
 # Test: GET /actividades
 echo "Test: GET /actividades"
-response=$(curl -s -o /dev/null -w "%{http_code}" "$BASE_URL")
+response=$(curl -u "$USERNAME:$PASSWORD" -s -o /dev/null -w "%{http_code}" "$BASE_URL")
 if [[ "$response" -eq 200 ]]; then
     echo "GET /actividades: OK (Status Code: $response)"
 else
