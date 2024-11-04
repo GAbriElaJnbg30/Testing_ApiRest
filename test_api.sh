39a74263024b88cc853113dc77415de636f628b4
GAbriElaJnbg30 <21240200@leon.tecnm.mx>
2024-11-03 22:24:57 -0600

    Ajustes de test_api.sh

--- test_api.sh
+++ test_api.sh
@@ -9,16 +9,40 @@ BASE_URL="http://localhost:8080/actividades"
 
 # Test: GET /actividades
 echo "Test: GET /actividades"
-curl -u "$USERNAME:$PASSWORD" -X GET "$BASE_URL"
+response=$(curl -u "$USERNAME:$PASSWORD" -X GET "$BASE_URL")
+echo "$response"
+
+# Comprobar si hubo un error de autenticación
+if [[ "$response" == *"Unauthorized"* ]]; then
+    echo "Error: No autorizado para acceder al endpoint GET /actividades"
+fi
 
 # Test: POST /actividades
 echo -e "\nTest: POST /actividades"
-curl -u "$USERNAME:$PASSWORD" -X POST "$BASE_URL" -H "Content-Type: application/json" -d '{"nombre": "Nuevo Actividad", "descripcion": "Descripción de la nueva actividad"}'
+response=$(curl -u "$USERNAME:$PASSWORD" -X POST "$BASE_URL" -H "Content-Type: application/json" -d '{"nombre": "Nueva Actividad", "descripcion": "Descripción de la nueva actividad"}')
+echo "$response"
+
+# Comprobar si hubo un error de autenticación
+if [[ "$response" == *"Unauthorized"* ]]; then
+    echo "Error: No autorizado para acceder al endpoint POST /actividades"
+fi
 
 # Test: PUT /actividades/1
 echo -e "\nTest: PUT /actividades/1"
-curl -u "$USERNAME:$PASSWORD" -X PUT "$BASE_URL/1" -H "Content-Type: application/json" -d '{"nombre": "Actividad Modificada", "descripcion": "Descripción modificada de la actividad"}'
+response=$(curl -u "$USERNAME:$PASSWORD" -X PUT "$BASE_URL/1" -H "Content-Type: application/json" -d '{"nombre": "Actividad Modificada", "descripcion": "Descripción modificada de la actividad"}')
+echo "$response"
+
+# Comprobar si hubo un error de autenticación
+if [[ "$response" == *"Unauthorized"* ]]; then
+    echo "Error: No autorizado para acceder al endpoint PUT /actividades/1"
+fi
 
 # Test: DELETE /actividades/1
 echo -e "\nTest: DELETE /actividades/1"
-curl -u "$USERNAME:$PASSWORD" -X DELETE "$BASE_URL/1"
+response=$(curl -u "$USERNAME:$PASSWORD" -X DELETE "$BASE_URL/1")
+echo "$response"
+
+# Comprobar si hubo un error de autenticación
+if [[ "$response" == *"Unauthorized"* ]]; then
+    echo "Error: No autorizado para acceder al endpoint DELETE /actividades/1"
+fi
