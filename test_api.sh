eee40cf3f0d789a8b8ca301396d75027fe9533a8
GAbriElaJnbg30 <21240200@leon.tecnm.mx>
2024-11-03 22:29:31 -0600

    Ajustes de test_api.sh

--- test_api.sh
+++ test_api.sh
@@ -1,48 +1,13 @@
 #!/bin/bash
 
-# Credenciales
-USERNAME="goretti"
-PASSWORD="gn300803"
-
 # Endpoint base
 BASE_URL="http://localhost:8080/actividades"
 
 # Test: GET /actividades
 echo "Test: GET /actividades"
-response=$(curl -u "$USERNAME:$PASSWORD" -X GET "$BASE_URL")
-echo "$response"
-
-# Comprobar si hubo un error de autenticación
-if [[ "$response" == *"Unauthorized"* ]]; then
-    echo "Error: No autorizado para acceder al endpoint GET /actividades"
-fi
-
-# Test: POST /actividades
-echo -e "\nTest: POST /actividades"
-response=$(curl -u "$USERNAME:$PASSWORD" -X POST "$BASE_URL" -H "Content-Type: application/json" -d '{"nombre": "Nueva Actividad", "descripcion": "Descripción de la nueva actividad"}')
-echo "$response"
-
-# Comprobar si hubo un error de autenticación
-if [[ "$response" == *"Unauthorized"* ]]; then
-    echo "Error: No autorizado para acceder al endpoint POST /actividades"
-fi
-
-# Test: PUT /actividades/1
-echo -e "\nTest: PUT /actividades/1"
-response=$(curl -u "$USERNAME:$PASSWORD" -X PUT "$BASE_URL/1" -H "Content-Type: application/json" -d '{"nombre": "Actividad Modificada", "descripcion": "Descripción modificada de la actividad"}')
-echo "$response"
-
-# Comprobar si hubo un error de autenticación
-if [[ "$response" == *"Unauthorized"* ]]; then
-    echo "Error: No autorizado para acceder al endpoint PUT /actividades/1"
-fi
-
-# Test: DELETE /actividades/1
-echo -e "\nTest: DELETE /actividades/1"
-response=$(curl -u "$USERNAME:$PASSWORD" -X DELETE "$BASE_URL/1")
-echo "$response"
-
-# Comprobar si hubo un error de autenticación
-if [[ "$response" == *"Unauthorized"* ]]; then
-    echo "Error: No autorizado para acceder al endpoint DELETE /actividades/1"
+response=$(curl -s -o /dev/null -w "%{http_code}" "$BASE_URL")
+if [[ "$response" -eq 200 ]]; then
+    echo "GET /actividades: OK (Status Code: $response)"
+else
+    echo "GET /actividades: Failed (Status Code: $response)"
 fi
